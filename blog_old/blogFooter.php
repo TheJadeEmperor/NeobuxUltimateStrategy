3af947ea94b324ed7d0b8869fd279934878e556d
TheJadeEmperor <louie.online.acc@gmail.com>
2017-10-07 21:34:13 -0400

    archive old blog

---
+++ blog_old/blogFooter.php
@@ -0,0 +1,206 @@
+<br /><br />
+<center>
+<div class="orderForm" id="optin">
+    <table>
+    <tr>
+        <td>
+            <img src="images/splash/nusXSmall.jpg" alt="NUS Cover" title="NUS Cover" />
+        </td>
+        <td width="30px"></td>
+        <td align="center">
+            <h2>Get Your Free Neobux Report!</h2>
+            
+            <p>Just Enter Your Email Address Below </p>
+            <form method=post action="http://www.trafficwave.net/cgi-bin/autoresp/inforeq.cgi">
+            <table align="center" cellpadding="1">
+            <tr>
+                <td align="left"></td>
+                <td><input type=text id="da_email" name="da_email" class="input" size="25" 
+                value="Enter your email" onclick="if(this.value=='Enter your email') this.value=''" />
+                </td>
+            </tr><tr>
+                <td colspan="2" align="center">
+                
+                    <input type="image" src="images/sales/getAccessNow.png" name="subscribe" width="200px"  
+                    title="Subscribe Now!"></td>
+            </tr>
+            </table>
+            
+            <input type=hidden id="da_name" name="da_name" value="PTC User">
+            <input type=hidden name="da_cust1" value="blog post" />
+            <input type=hidden name="da_cust2" value="">
+            <input type=hidden name="trwvid" value="theemperor">
+            <input type=hidden name="series" value="nusnewsletter">
+            <input type=hidden name="subscrLandingURL" value="http://neobuxultimatestrategy.com/redirect.php?url=basics">
+            <input type=hidden name="confirmLandingURL" value="http://neobuxultimatestrategy.com/redirect.php?url=basics">
+            </form>
+            
+            <br />
+            <span class="note">We hate spam and will never sell your email address to others. </span>
+        </td>
+    </tr>
+    </table>
+</div>	
+</center>
+
+<?
+$selP = 'SELECT * FROM posts WHERE status <> "I" ORDER BY postedOn DESC LIMIT 10';
+$resP = mysql_query($selP, $conn) or die(mysql_error());
+
+while($p = mysql_fetch_assoc($resP)) {
+    $recentPosts .= '<p align="left"><a href="./?p='.$p['url'].'">'.shortenText($p['subject'], 35).'</a></p>'; 
+}
+?>	
+
+<p>&nbsp;</p>
+	 
+</td>
+<td width="20px"></td>
+<td align="center" id="rightCol">
+<br />
+<center>
+<div class="optin" id="optin">
+    <table width="310px" cellpadding="0">
+        <tr>
+            <td align="center">
+        
+				<h1>Get 1000+ Downlines Using Our System!</h1>
+			
+				<p class="note"><input type=checkbox checked>Yes, please subscribe me and send 
+							me a copy of the <br><b>free Neobux Basics eBook!</b></p>
+				
+				<br />
+				<form method=post action="http://www.trafficwave.net/cgi-bin/autoresp/inforeq.cgi">
+    
+                    <input type=text class="input" size="25" id="da_email" name="da_email" value="Your Email Address"
+                    onclick="if(this.value=='Your Email Address') this.value='';">
+
+                    <input type=image src="images/sales/getAccessNow.png"
+                        value=" >> Subscribe! << " onclick="submit();" width="250px" />            
+
+                    <input type="hidden" id="da_name" name="da_name" value="PTC User" />
+                    <input type=hidden name="da_cust1" value="blog menu">
+                    <input type=hidden name="da_cust2" value="<?=$_SERVER[HTTP_REFERER]?>">
+                    <input type=hidden name="trwvid" value="theemperor">
+                    <input type=hidden name="series" value="nusnewsletter">
+                    <input type=hidden name="subscrLandingURL" value="<?=$subscrLandingURL?>">
+                    <input type=hidden name="confirmLandingURL" value="<?=$confirmLandingURL?>">
+                </form>
+		
+                <p><span class="note">We hate spam and will never sell your email address <br />to others. 
+                        All opt-ins are completely optional.</span></p>
+		 
+            </td>
+		</tr>
+	</table>
+	</div>
+	</center>
+	<br /><br />
+	
+	<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+        <fb:like-box href="http://www.facebook.com/NeobuxUltimateStrategy" width="320" show_faces="true"    
+        border_color="gray" stream="false" header="true"></fb:like-box>
+    
+	<br /><br />
+
+	<div class="moduleBlue"><h1>Recent Posts</h1><div>
+		<?=$recentPosts?>
+		<center><p><a href="./?action=posts">Show all posts</a></p></center>
+	</div></div>
+	
+	<br /><br />
+        
+
+	<div class="moduleBlue"><h1>Current Products </h1>
+		<div>
+
+            <p><b>Neobux Basics</b></p>
+            <p>To get this free ebook just sign up to our newsletter.</p>
+
+            <p><b><a href="./">Neobux Ultimate Strategy</a></b></p>
+
+            <p>The ultimate strategy guide to making money with Neobux, comes in PDF format and 
+            video tutorials. </p>
+
+            <p><b><a href="./minisite">PTC Mini-Sites</a></b></p>
+
+            <p>Get your own PTC website for a fraction of the cost of regular websites</p>
+
+            </div>
+        </div>
+        
+        <br /><br />
+		
+        <div class="moduleBlue"><h1>Neobux Ultimate Strategy</h1>
+			<div>
+			<center>
+				<p><a href="./"><img src="./images/sales/nus.jpg" title="Neobux Ultimate Strategy" width="150px" /></a> </p>
+				<p>Make $20 to $30 a day with Neobux </p>
+			</center>
+			</div>
+		</div>
+		   
+	    <br /><br />
+        
+        <div class="moduleBlue"><h1>NUS Video Course</h1>
+		<div>
+        <center>
+            <p><a href="./"><img src="./images/guide/cd.jpg" title="NUS Video Course" width="180px" /></a> </p>
+            <p>The Neobux Ultimate Strategy <br />in video format</p>
+        </center>
+        </div>
+		</div>
+        
+        <br /><br />
+       
+        <div class="moduleBlue"><h1>Recommended Sites</h1>
+		<div>
+            <p><a href="http://vector.me" title="Free Clipart" target="_blank">Free Clipart</a></p>
+
+            <p><a href="http://bestpayingsites.com/ppbooster" target="_blank">Paypal Booster</a></p>
+            
+            <p><a href="http://bestpayingsites.com/" target="_blank">Email Profit System</a></p>
+                
+            <p><a href="http://www.neobux.com/forum/?/15/229319/Thanks-to-Neobux-Ultimate-Strategy/" target="_blank" rel="nofollow">Neobux Forum 1</a></p>
+            
+            <p><a href="http://www.neobux.com/forum/?/7/213100/Neobux-Ultimate-Strategy/" target="_blank" rel="nofollow">Neobux Forum 2</a></p>            
+        </div>
+        </div>
+	</td>
+    </tr>
+</table>
+          
+    <p>&nbsp;</p> <hr />
+
+    <div class="footer">
+        <p>Copyright &copy; <?=date('Y', time())?> NUS Blog - All rights reserved</p>
+
+        <div id="eXTReMe"><a href="http://extremetracking.com/open?login=richptc" target="_BLANK" rel="nofollow">
+        <img src="http://t1.extreme-dm.com/i.gif" style="border: 0;"
+        height="38" width="41" id="EXim" alt="eXTReMe Tracker" /></a>
+        <script type="text/javascript"><!--
+        EXref="";top.document.referrer?EXref=top.document.referrer:EXref=document.referrer;//-->
+        </script><script type="text/javascript"><!--
+        var EXlogin='richptc' // Login
+        var EXvsrv='s10' // VServer
+        EXs=screen;EXw=EXs.width;navigator.appName!="Netscape"?
+        EXb=EXs.colorDepth:EXb=EXs.pixelDepth;EXsrc="src";
+        navigator.javaEnabled()==1?EXjv="y":EXjv="n";
+        EXd=document;EXw?"":EXw="na";EXb?"":EXb="na";
+        EXref?EXref=EXref:EXref=EXd.referrer;
+        EXd.write("<img "+EXsrc+"=http://e1.extreme-dm.com",
+        "/"+EXvsrv+".g?login="+EXlogin+"&amp;",
+        "jv="+EXjv+"&amp;j=y&amp;srw="+EXw+"&amp;srb="+EXb+"&amp;",
+        "l="+escape(EXref)+" height=1 width=1>");//-->
+        </script><noscript><div id="neXTReMe"><img height="1" width="1" alt=""
+        src="http://e1.extreme-dm.com/s10.g?login=richptc&amp;j=n&amp;jv=n" />
+        </div></noscript></div>
+
+    </div><!--footer-->
+</div><!-- container -->
+</div><!-- wrapper -->
+
+<br />
+
+</body>
+</html>
---
+++ blog_old/post.php
@@ -0,0 +1,87 @@
+<?php
+$url = $_GET['p']; 
+$websiteURL = $val['websiteURL']; 
+
+//get post details
+$sel = 'SELECT *, date_format(postedOn, "%m/%d/%Y %h:%i %p") AS postedOn, p.id AS id
+FROM posts p WHERE p.url="'.$url.'" LIMIT 1';
+$res = mysql_query($sel, $conn) or die(mysql_error());
+
+if(mysql_num_rows($res) > 0) {
+    $p = mysql_fetch_assoc($res);
+
+    $postID = $p['id']; 
+    $p['post'] = stripslashes($p['post']);
+    $p['subject'] = stripslashes($p['subject']);
+    $postLink = $websiteURL.'/'.$url;
+
+    if($p['subject']=='')
+        $p['subject'] = '[No Subject]';
+
+    if($p['post']=='')
+        $p['post'] = '[No content]';
+
+    $keywords = explode(', ', $p[tags]);
+    $tagArray = array();
+	
+    foreach($keywords as $piece) {
+		array_push($tagArray, '<li><b><a class="button special" href="./?p='.$url.'" title="'.$piece.'">'.$piece.'</a></b></li>'); 
+    }
+
+    $tags = implode(' ', $tagArray); 
+
+	$num = 0;
+}
+
+
+if(mysql_num_rows($res) > 0) {
+    
+    $subject = $p['subject']; 
+    
+    $postLink = 'http://neobuxultimatestrategy.com/?p='.$url;
+    
+    echo '<header class="major">
+		<a href="'.$postLink.'" class="postTitle" title="'.$p['subject'].'"><h2>'.$p['subject'].'</h2></a>
+	<p>By <a href="./">Admin</a> on '.$p['postedOn'].'</p>
+
+	<span class="st_twitter_vcount" displayText="Tweet"></span>
+    <span class="st_email_vcount" displayText="Email"></span>
+    <span class="st_facebook_vcount" displayText="Facebook"></span>
+    <span class="st_sharethis_vcount" displayText="ShareThis"></span>
+    <span class="st_fblike_vcount" st_title="'.$subject.'" st_url="'.$postLink.'" displayText="share"></span>
+	</header>
+	
+    <p>&nbsp;</p>'; 
+
+
+    if($p['useHTMLFile'] == 'on') {
+        include('blog/'.$url.'.html');
+    }
+    else {
+        echo $p['post']; 
+    }
+    
+    echo '<p>&nbsp; </p>
+    <span class="st_twitter_vcount" displayText="Tweet"></span>
+    <span class="st_email_vcount" displayText="Email"></span>
+    <span class="st_facebook_vcount" displayText="Facebook"></span>
+    <span class="st_sharethis_vcount" displayText="ShareThis"></span>
+    <span class="st_fblike_vcount" st_title="'.$subject.'" st_url="'.$postLink.'" displayText="share"></span>
+
+    <table width="100%" id="comments">
+	<tr valign="top">
+	<td align="left"> 
+		<p><ul class="actions">'.$tags.'</ul></p>
+	</td>
+		<td align="right" width="150px"><p>Comments ('.$num.') </p></td>
+	</tr>
+	</table> 
+	<p>&nbsp;</p>'; 
+
+    echo $_SESSION['msg']; 
+}
+else {
+    echo 'No post by that title exists'; 
+}
+
+?>
