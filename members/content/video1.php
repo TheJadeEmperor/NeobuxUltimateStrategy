7c43f064b017b4ef9b75b6277415d96616adb9f8
TheJadeEmperor <benjamin_louie@drugfree.org>
2018-09-23 15:56:42 -0400

    Https videos

--- members/content/video1.php
+++ members/content/video1.php
@@ -10,7 +10,7 @@ share the links to the public - it won't be fair to those who have paid for them
 <h2>Module 1 - Intro - A</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/kZyh7umgx0E',
+    'src' => 'https://www.youtube.com/embed/kZyh7umgx0E',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -22,7 +22,7 @@ echo embedYoutubeVideo($options); ?>
 <h2>Module 2-A - TrafficWave - Part 1</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/Hnj3Dyta1AE',
+    'src' => 'https://www.youtube.com/embed/Hnj3Dyta1AE',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -34,7 +34,7 @@ echo embedYoutubeVideo($options);
 <h2>Module 2-B - TrafficWave - Part 2</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/MJmCfCa87F0',
+    'src' => 'https://www.youtube.com/embed/MJmCfCa87F0',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -46,7 +46,7 @@ echo embedYoutubeVideo($options);
 <h2>Module 2-C - TrafficWave - Part 3</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/8ZSoZnQLsQA',
+    'src' => 'https://www.youtube.com/embed/8ZSoZnQLsQA',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -58,7 +58,7 @@ echo embedYoutubeVideo($options);
 <h2>Module 3-A - TrafficWave - Part 1</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/05Ib7LYn4XY',
+    'src' => 'https://www.youtube.com/embed/05Ib7LYn4XY',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -71,7 +71,7 @@ echo embedYoutubeVideo($options);
 
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/O_C5xgIF858',
+    'src' => 'https://www.youtube.com/embed/O_C5xgIF858',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
--- members/content/video2.php
+++ members/content/video2.php
@@ -6,7 +6,7 @@
 <h2>Module 4-A - DownlineRefs - Part 1</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/H9Twb7fqEZ4',
+    'src' => 'https://www.youtube.com/embed/H9Twb7fqEZ4',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -17,7 +17,7 @@ echo embedYoutubeVideo($options); ?>
 <h2>Module 4-B - DownlineRefs - Part 2</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/GU7NB-l70nY',
+    'src' => 'https://www.youtube.com/embed/GU7NB-l70nY',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -28,7 +28,7 @@ echo embedYoutubeVideo($options); ?>
 <h2>Module 5-A - EasyHits4U - Part 1</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/rbCjSxdGSww',
+    'src' => 'https://www.youtube.com/embed/rbCjSxdGSww',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -40,7 +40,7 @@ echo embedYoutubeVideo($options); ?>
 <h2>Module 5-B - EasyHits4U - Part 2</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/wuToLgleZ_4',
+    'src' => 'https://www.youtube.com/embed/wuToLgleZ_4',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
@@ -52,7 +52,7 @@ echo embedYoutubeVideo($options); ?>
 <h2>Module 5-C - EasyHits4U - Part 3</h2>
 <? 
 $options = array(
-    'src' => 'http://www.youtube.com/embed/g4U1_PDFe-M',
+    'src' => 'https://www.youtube.com/embed/g4U1_PDFe-M',
     'width' => '420',
     'height' => '315',
     'options' => 'rel=0'
