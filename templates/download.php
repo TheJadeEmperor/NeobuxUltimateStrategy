946b5af69923e98283d7048c9edd794f8b1644ed
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-01-07 23:23:34 -0500

    mysqli download.php

--- templates/download.php
+++ templates/download.php
@@ -1,12 +1,13 @@
 <?php
-$transID = $_GET['id'];
+$transID = $_GET['id']; 
 
 //check for transaction in db
-$selS = 'SELECT *, date_format(purchased, "%m/%d/%Y") AS salesDate,
-date_format(expires, "%m/%d/%Y") AS expiresDate FROM sales WHERE transID = "'.$_GET['id'].'" AND productID="'.$productID.'"';
-$resS = $conn->query($selS);
+$selS = 'select *, date_format(purchased, "%m/%d/%Y") as salesDate,
+date_format(expires, "%m/%d/%Y") as expiresDate from sales where transID = "'.$_GET['id'].'"
+and productID="'.$productID.'"';
+$conn->query($selS);
 
-$sales = mysqli_num_rows($resS);
+$sales = $resS->num_rows;
 
 if($s = $resS->fetch_array()) {
     $firstName = $s['firstName'];
@@ -14,13 +15,14 @@ if($s = $resS->fetch_array()) {
     $salesDate = $s['salesDate'];  
     $expiresDate = $s['expiresDate'];
     $payerEmail = $s['payerEmail'];
- 
+    
     if(!isset($expiresDate))
         $expiresDate = $salesDate; 
 }
 
-if($oto == 'Y' && !$_POST['skipOTO']) { //one time offer 
+if($oto == 'Y' && !$_POST['skipOTO']) { //upsell 
     $fileName = 'oto.html';
+
 }
 else {
     if($sales > 0) { //valid sale
@@ -29,70 +31,73 @@ else {
         $expiresDate = strtotime($expiresDate); 
         $expireSeconds = $expires * 60 * 60; 
 		
-        if($transID == 'vipuser') { //no expiration date for VIP user 
+        if($transID == 'vipuser') { //no expiration date for VIP user
             $today = 0;
-            $expiresDate = 0;
+            $expiresDate = 0; 
         }
-        
-        if(($today) <= ($expiresDate + $expireSeconds)) { 
-            //check for existing account
-            $selU = 'SELECT * FROM users WHERE email="'.$payerEmail.'" OR paypal="'.$payerEmail.'"';
-            $resU = $conn->query($selU);
-          
-            if(mysqli_num_rows($resU) == 0) { //no account exists
+		
+        if(($today) <= ($expiresDate + $expireSeconds)) {
+            $opt = array(
+                'tableName' => 'users',
+                'cond' => 'WHERE email="'.$payerEmail.'" OR paypal="'.$payerEmail.'"'
+            );           
+            $resU = dbSelectQuery($opt);
+             
+            if($resU->num_rows == 0) { //no account exists
            
                 //generate random password
                 $password = genString(8);
                 
                 //insert email & password into db
-                $insI = 'insert into users (
-                paypal,
-                email,
-                password,
-                joinDate
-                ) values (
-                "'.$payerEmail.'",
-                "'.$payerEmail.'",
-                "'.$password.'",
-                now() )';
-
-                $resI = $conn->query($insI);
+                $opt = array(
+                    'tableName' => 'users',
+                    'dbFields' => array(
+                        'paypal' => $payerEmail,
+                        'email' => $payerEmail,
+                        'password' => $password,
+                        'joinDate' => 'now()'
+                    )
+                );
+               
+                dbInsert($opt);
             }
-            else  { //existing account 
-           
-                $u = $resU->fetch_array(); 
-              
+            else { //existing account 
+                //$u = mysql_fetch_assoc($resU); 
+                $u = $resU->fetch_assoc();
                 $password = $u['password'];
             } 
             
-            //multiple downloads
-            $selD = 'SELECT * FROM downloads WHERE productID="'.$productID.'" ORDER BY name';
-            $resD = $conn->query($selD);
-           
-            if(mysqli_num_rows($resD) > 0) {
+            $opt = array(
+                'tableName' => 'downloads',
+                'cond' => 'WHERE productID="'.$productID.'" ORDER BY name'
+            ); 
+            
+            $resD = dbSelectQuery($opt);
+            
+            if($resD->num_rows > 0) {
                 $downloadContent = '<table>';
-                while($d = mysql_fetch_assoc($resD))  {
+                while($d = $resD->fetch_assoc()) {
                     $downloadContent .= '<tr>
-                    <td>'.$d['name'].'</td>
-                    <td>
-                    <form method="post"><input type="submit" name="dl" value="Download"/>
-                    <input type="hidden" name="url" value="'.$d['url'].'" />
-                    </form>
+                    <td>'.$d[name].'</td>
+                    <td><form method="POST"><input type="submit" name="dl" value="Download" />
+                        <input type="hidden" name="url" value="'.$d[url].'" /></form>
                     </td>';
                 }    
                 $downloadContent .= '</table>';
             }
-           
+            
             $fileName = 'download.html';
         } 
         else {
             $fileName = 'expired.html';
         }
     }
-    else  { //invalid sale
-  
+    else { //invalid sale
         $fileName = 'invalid.html';
     }
-}       
+}  
+//echo __LINE__.' ';
+
+//echo $fileName;
 include($fileName); 
 ?>
