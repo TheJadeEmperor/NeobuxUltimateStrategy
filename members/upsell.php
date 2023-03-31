e4411c1be070579055846c90644b02e714f11b01
TheJadeEmperor <louie.online.acc@gmail.com>
2023-03-30 23:38:35 -0400

    Update upsell.php

--- members/upsell.php
+++ members/upsell.php
@@ -1,13 +1,13 @@
 <?php
 
 ////
-$myfile = fopen("testfile.txt", "w");
-fwrite($myfile, __LINE__.' ');
+$my = fopen("testfile1.txt", "w");
+fwrite($my, __LINE__.' ');
 ///
 if(!$_SESSION['login']['id'])
     header('Location: index.php');
 //
-fwrite($myfile, __LINE__.' ');
+fwrite($my, __LINE__.' ');
 
 function getProduct($productID) {
     global $conn; 
@@ -19,7 +19,7 @@ function getProduct($productID) {
     return $p; 
 }
 
-fwrite($myfile, __LINE__.' ');
+fwrite($my, __LINE__.' ');
 
 $skipButton = '
 <center>
@@ -33,41 +33,46 @@ $transition = '<meta http-equiv="refresh" content="1;url=./?action=affcenter">
     <h1>Logging you in</h1>
     <img src="../images/waiting.gif" /><p>&nbsp;</p><p>&nbsp;</p>
 </center>';
+ 
 
-$val['upsellA'] = array(
-    'productID' => 3, //PTC Mini site
-    'upsellFile' => 'upsellPTCMiniSite.html'
-); 
+$val['upID'] = 3;
+$val['upFile'] = 'content/upsellPTCMiniSite.html';
 
-fwrite($myfile, __LINE__.' ');
+//echo $val['upID]']. ' '.$val['upFile'];
 
-if($val['memAreaUpsell'] == 'on') {
+ 
 
-    $selUP = 'SELECT id from sales where payerEmail="'.$u['paypal'].'" and productID="'.$val['memUpsellProductID'].'"';
+fwrite($my, __LINE__.' ');
+
+if($val['upID']) {
+
+    $selUP = 'SELECT id from sales where payerEmail="'.$u['paypal'].'" and productID="'.$val['upsell']['productID'].'"';
     $resUP = $conn->query($selUP);
+
+    fwrite($my, __LINE__.' '.$val['upFile'] );
 
     if(mysqli_num_rows($resUP) == 0) { //if user is not a customer         
 
-        include('content/'.$val['memUpsellFile']);
-        include('content/upsellPTCMiniste.html');
+//        include('content/'.$val['memUpsellFile']);
+        include( $val['upFile'] );
         
-        fwrite($myfile, __LINE__.' '.$val['memUpsellFile']);
+        fwrite($my, __LINE__.' '.$val['upFile'] );
     }
     else { //if user is a customer, skip upsell 
         $_SESSION['login']['skipUpsell'] = true;
         echo $transition; 
     }
 
-    fwrite($myfile, __LINE__);
+    fwrite($my, __LINE__);
 
 }
 else { //if upsell is disabled 
 
-    fwrite($myfile, __LINE__);
+    fwrite($my, __LINE__);
 
     $_SESSION['login']['skipOTO'] = true;
     echo $transition; 
 }
-fwrite($myfile, __LINE__);
+fwrite($my, __LINE__);
 
 ?>
