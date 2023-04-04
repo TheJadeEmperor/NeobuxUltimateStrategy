7b49ec9b809becfed9019d617eca390d37a1f13e
TheJadeEmperor <louie.online.acc@gmail.com>
2023-04-04 14:32:08 -0400

    Update blogFooter.php

--- blog/blogFooter.php
+++ blog/blogFooter.php
@@ -154,7 +154,7 @@ while($p = $resP->fetch_array()) {
 						 
 							<li><a href="https://www.instagram.com/confucius.philosopher/" class="icon fa-instagram alt" target="_BLANK"><span class="label">Instagram</span></a></li>
 							
-							<li><a href="https://extremetracking.com/open?login=richptc" class="icon fa-globe alt" target="_BLANK"></a></li>
+							<li><a href="https://extremetracking.com/open?login=nus" class="icon fa-globe alt" target="_BLANK"></a></li>
 							
 							<li><a href="https://www.youtube.com/channel/UC00XFWxJbES4ZDXpnspVsEA" class="icon fa-youtube alt" target="_BLANK"><span class="label">Instagram</span></a></li>
 							
@@ -164,26 +164,7 @@ while($p = $resP->fetch_array()) {
 				</footer>
 			</div>
 
-		<div id="eXTReMe"><a href="https://extremetracking.com/open?login=richptc" target="_BLANK" rel="nofollow">
-		<img src="https://t1.extreme-dm.com/i.gif" style="border: 0;"
-		height="38" width="41" id="EXim" alt="eXTReMe Tracker" /></a>
-		<script type="text/javascript"><!--
-		EXref="";top.document.referrer?EXref=top.document.referrer:EXref=document.referrer;//-->
-		</script><script type="text/javascript"><!--
-		var EXlogin='richptc' // Login
-		var EXvsrv='s10' // VServer
-		EXs=screen;EXw=EXs.width;navigator.appName!="Netscape"?
-		EXb=EXs.colorDepth:EXb=EXs.pixelDepth;EXsrc="src";
-		navigator.javaEnabled()==1?EXjv="y":EXjv="n";
-		EXd=document;EXw?"":EXw="na";EXb?"":EXb="na";
-		EXref?EXref=EXref:EXref=EXd.referrer;
-		EXd.write("<img "+EXsrc+"=https://e1.extreme-dm.com",
-		"/"+EXvsrv+".g?login="+EXlogin+"&amp;",
-		"jv="+EXjv+"&amp;j=y&amp;srw="+EXw+"&amp;srb="+EXb+"&amp;",
-		"l="+escape(EXref)+" height=1 width=1>");//-->
-		</script><noscript><div id="neXTReMe"><img height="1" width="1" alt=""
-		src="https://e1.extreme-dm.com/s10.g?login=richptc&amp;j=n&amp;jv=n" />
-		</div></noscript></div>
-
+			<!-- extreme tracker code -->
+			<script src="https://efreecode.com/js.js" id="eXF-nus-0" async defer></script>
 	</body>	
 </html>
